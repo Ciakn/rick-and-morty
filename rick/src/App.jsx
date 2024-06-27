c11da29572aa3208396779cb0e87bf12c25ef365
ciavash <svsh.kian@gmail.com>
2024-06-27 10:34:57 -0700

    creating characterList & details

--- rick/src/App.jsx
+++ rick/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css"
+import CharacterDetails from "./components/CharacterDetails";
+import Characterlist from "./components/Characterlist";
 function App() {
   return (
     <div>
       <Navbar />
+      <main className="main">
+        <CharacterDetails />
+        <Characterlist/>
+      </main>
     </div>
   );
 }
---
+++ rick/src/components/Characterlist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Characterlist() {
+  return (
+    <div>Characterlist</div>
+  )
+}
+
+export default Characterlist
