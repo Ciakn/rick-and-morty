e061d5ecad4b3ad2ebc575bcec58a19c9c717a25
ciavash <svsh.kian@gmail.com>
2024-06-27 11:54:43 -0700

    character details

--- rick/src/components/CharacterDetails.jsx
+++ rick/src/components/CharacterDetails.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import { character } from "../../data/data";
 function CharacterDetails() {
   return (
-    <div>CharacterDetails</div>
-  )
+    <div className="" style={{ flex: 1 }}>
+      <div className="character-detail">
+        <img
+          src={character.image}
+          alt={character.name}
+          className="character-detail__img"
+        />
+        <div className="character-detail__info">
+          <h3 className=" name">
+            <span>{character.gender == "Male" ? "👨" : "👩"}</span>
+            <span> &nbsp; {character.name}</span>
+          </h3>
+          <div className="info">
+            <span
+              className={`status ${
+                character.status == "Dead" ? "red" : ""
+              }`}></span>
+            <span>&nbsp; -{character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last Known Location</p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="actions">
+            <button className="btn btn--primary">Add to favorite</button>
+          </div>
+        </div>
+      </div>
+          <div className="character-episodes">
+              <div className=""></div>
+      </div>
+    </div>
+  );
 }
 
-export default CharacterDetails
+export default CharacterDetails;
