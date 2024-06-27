cc1ceb5fdb843442880c0d18370dfddf6bbffb42
ciavash <svsh.kian@gmail.com>
2024-06-27 10:29:15 -0700

    building Navbar & adding Styies

---
+++ rick/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {HeartIcon} from "@heroicons/react/24/outline"
+function Navbar() {
+  return (
+      <nav className="navbar">
+          
+          <div className="navbar__logo">LOGO ##</div>
+          <input type="text" className="text-field" placeholder="search..." />
+          <div className="navbar__result"> found X Charecter</div>
+          <button className="heart"> <HeartIcon className="icon"/><span className="badge">4</span> </button>
+    </nav>
+  );
+}
+
+export default Navbar;
