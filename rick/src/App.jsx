7229d392c6e1f1f881ddd58a928a68cc7dda998f
ciavash <svsh.kian@gmail.com>
2024-06-28 11:33:00 -0700

    getting data by fetch & useEffect

--- rick/src/App.jsx
+++ rick/src/App.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
-import "./App.css"
+import "./App.css";
 import CharacterDetails from "./components/CharacterDetails";
 import Characterlist from "./components/Characterlist";
-import {character, allCharacters} from "../data/data"
+import { character, allCharacters } from "../data/data";
 function App() {
-  const [characters , setCharacters]=useState(allCharacters)
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => setCharacters(data.results.slice(0,5)));
+  }, []);
   return (
     <div>
-      <Navbar numOfCharacters = {characters.length}/>
+      <Navbar numOfCharacters={characters.length} />
       <main className="main">
-        <Characterlist characters={ characters} />
+        <Characterlist characters={characters} />
         <CharacterDetails />
       </main>
     </div>
